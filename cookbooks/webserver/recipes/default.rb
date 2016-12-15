44b1406b81fb72307c136d75bae658e905815576
root <root@ip-172-31-53-252.ec2.internal>
2016-12-15 21:14:13 +0000

    init

--- cookbooks/webserver/recipes/default.rb
+++ cookbooks/webserver/recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+package 'nginx'
+service 'nginx' do
+  action [:enable, :start]
+end
+
+template '/etc/nginx/nginx.conf' do
+  source 'nginx.conf.erb'
+end
