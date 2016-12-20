e30860e7416f7707649da05fd4046e2a5319a89e
root <root@web1.localdomain>
2016-12-20 19:52:15 +0000

    cookbook had an older link to dotnet download

--- dotnetcore/attributes/default.rb
+++ dotnetcore/attributes/default.rb
@@ -9,5 +9,5 @@
 
 default['dotnetcore']['package']['name'] = 'dotnet-dev-1.0.0-preview2-003131'
 default['dotnetcore']['package']['source_url'] = 'https://download.microsoft.com/download/5/5/7/557D02A5-C3D0-4EF6-A570-4F75CD0DA5BF/DotNetCore.1.0.1-WindowsHosting.exe'
-default['dotnetcore']['package']['tar'] = 'https://go.microsoft.com/fwlink/?LinkID=827529'
+default['dotnetcore']['package']['tar'] = 'https://go.microsoft.com/fwlink/?LinkID=835019'
 default['dotnetcore']['apt_package_source'] = 'http://apt-mo.trafficmanager.net/repos/dotnet-release'
